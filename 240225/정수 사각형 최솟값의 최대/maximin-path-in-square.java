4edbb3d3f32af92275683d4cdd6114c13eb9a9e7
Areum Lee <90661870+souk0712@users.noreply.github.com>
2024-02-25 01:37:39 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 181ms, Memory: 12MB, Status: Passed - CodeTree

--- 240225/정수 사각형 최솟값의 최대/maximin-path-in-square.java
+++ 240225/정수 사각형 최솟값의 최대/maximin-path-in-square.java
@@ -11,15 +11,13 @@ public class Main {
         N = Integer.parseInt(br.readLine());
         map = new int[N][N];
         int[][] dp = new int[N][N];
-        int[][] min = new int[N][N];
         for(int i = 0;i<N;i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             for(int j = 0;j < N; j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
-                min[i][j] = Integer.MAX_VALUE;
             }
         }
-        dp[0][0] = min[0][0] = map[0][0];
+        dp[0][0] = map[0][0];
 
         for(int i = 0; i < N; i++){
             for(int j = 0; j < N; j++){
@@ -27,13 +25,10 @@ public class Main {
                     int mx = i + dx[k];
                     int my = j + dy[k];
                     if(mx < 0 || my < 0 || mx >= N || my >= N) continue;
-                    if(dp[mx][my] < dp[i][j] + map[mx][my]){
-                        dp[mx][my] = dp[i][j] + map[mx][my];
-                        min[mx][my] = Math.min(min[i][j], map[mx][my]);
-                    }
+                    dp[mx][my] = Math.max(Math.min(dp[i][j], map[mx][my]),dp[mx][my]);
                 }
             }
         }
-        System.out.println(min[N-1][N-1]);
+        System.out.println(dp[N-1][N-1]);
     }
 }
