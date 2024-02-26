7ad9fec28dbac2b6d37035dee6054947a0ba7ec6
Areum Lee <90661870+souk0712@users.noreply.github.com>
2024-02-26 23:59:54 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 433ms, Memory: 24MB, Status: Passed - CodeTree

--- 240226/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
+++ 240226/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
@@ -3,44 +3,49 @@ import java.util.*;
 
 public class Main {
     
+    static int N;
     static int[] dx = {0,1,0,-1};
     static int[] dy = {1,0,-1,0};
     static int[][] map, dp;
 
     public static void main(String[] args) throws Exception{
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
         map = new int[N][N];
         dp = new int[N][N];
         for(int i = 0; i < N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             for(int j = 0; j < N; j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
-                dp[i][j] = 1;
-            }
-        }
-
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < N; j++){
-                for(int k = 0; k < dx.length; k++){
-                    int mx = i + dx[k];
-                    int my = j + dy[k];
-                    if(mx < 0 || my < 0 || mx >= N || my >= N) continue;
-                    if(map[mx][my] > map[i][j]){
-                        dp[mx][my] = Math.max(dp[i][j] + 1, dp[mx][my]);
-                    }
-                }
-                            
+                dp[i][j] = -1;
             }
         }
         
         int max = 0;
         for(int i = 0; i < N; i++){
             for(int j = 0; j < N; j++){
-                max = Math.max(dp[i][j], max);      
+                max = Math.max(dfs(i,j), max);      
             }
         }
-        
+
         System.out.println(max);
+    }
+
+    static int dfs(int x, int y){
+        if(dp[x][y]!=-1){
+            return dp[x][y];
+        }
+
+        int res = 1;
+
+        for(int k = 0; k < dx.length; k++){
+            int mx = x + dx[k];
+            int my = y + dy[k];
+            if(mx < 0 || my < 0 || mx >= N || my >= N) continue;
+            if(map[mx][my] > map[x][y]){
+                res = Math.max(res, dfs(mx, my) + 1);
+            }
+        }
+        return dp[x][y] = res;
     }
 }
