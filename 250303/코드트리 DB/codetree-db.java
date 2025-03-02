de403c4bee1b208d540ec5f686f6b1682f7c3745
Areum Lee <90661870+souk0712@users.noreply.github.com>
2025-03-03 02:08:02 +0900

    Title: 코드트리 DB, Time: 5048ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

---
+++ 250303/코드트리 DB/codetree-db.java
@@ -0,0 +1,98 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int Q;
+    static HashMap<String, Integer> table;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Q = Integer.parseInt(br.readLine());
+        sb = new StringBuilder();
+        for(int i = 0; i < Q; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String keyword = st.nextToken();
+            switch(keyword){
+                case "init":
+                    init();
+                    break;
+                case "insert":
+                    insert(st.nextToken(), Integer.parseInt(st.nextToken()));
+                    break;
+                case "delete":
+                    delete(st.nextToken());
+                    break;
+                case "rank":
+                    rank(Integer.parseInt(st.nextToken()));
+                    break;
+                case "sum":
+                    sum(Integer.parseInt(st.nextToken()));
+                    break;
+            }
+        }
+        System.out.println(sb);
+    }
+
+    public static void init(){
+        table = new HashMap<>();
+    }
+
+    public static void insert(String name, int value){
+        if(table.containsKey(name) || table.containsValue(value)){
+            sb.append(0).append("\n");
+        }else{
+            table.put(name, value);
+            sb.append(1).append("\n");
+        }
+    }
+
+    public static void delete(String name){
+        if(!table.containsKey(name)){
+            sb.append(0).append("\n");
+        }else{
+            sb.append(table.remove(name)).append("\n");
+        }
+    }
+
+
+    public static void rank(int k){
+        if(table.size() < k){
+            sb.append("None").append("\n");
+            return;
+        }
+        List<Map.Entry<String, Integer>> entries = new ArrayList<>(table.entrySet());
+        entries.sort((o1, o2)-> Integer.compare(o1.getValue(), o2.getValue()));
+        sb.append(entries.get(k - 1).getKey()).append("\n");
+    }
+
+    public static void sum(int k){
+        int res = 0;
+        List<Map.Entry<String, Integer>> entries = new ArrayList<>(table.entrySet());
+        entries.sort((o1, o2)-> Integer.compare(o1.getValue(), o2.getValue()));
+        for(int i = 0; i < entries.size(); i++){
+            int value = entries.get(i).getValue();
+            if(value <= k){
+                res += value;
+                continue;
+            }
+            break;
+        }
+        sb.append(res).append("\n");
+    }
+
+    static class Data implements Comparable<Data> {
+        String name;
+        int value;
+
+        Data(String name, int value){
+            this.name = name;
+            this.value = value;
+        }
+
+        public int compareTo(Data o){
+            return Integer.compare(value, o.value);
+        }
+    }
+}
